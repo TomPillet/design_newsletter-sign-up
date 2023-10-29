908fd727c2dab723e1398f4c7a9f2d14c05dee21
SE63040 <tom.pillet-gaulon@capgemini.com>
2023-10-29 15:40:37 +0100

    responsive and desktop layout

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log(window.innerWidth);
+changeNewsletterImgSource();
+
+function changeNewsletterImgSource() {
+    document.querySelector('#newsletterImg').src = (window.innerWidth >= 1280) ? './assets/images/illustration-sign-up-desktop.svg' : './assets/images/illustration-sign-up-mobile.svg';
+}
+
+window.addEventListener('resize', () => {
+    changeNewsletterImgSource();
+})
