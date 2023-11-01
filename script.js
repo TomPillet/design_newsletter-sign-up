06a191fa2bd792ff882112706fe83423608866ab
Tom <tom963741@gmail.com>
2023-11-01 12:10:19 +0100

    rework input error logic and display

--- script.js
+++ script.js
@@ -8,19 +8,30 @@ function changeNewsletterImgSource() {
     document.querySelector('#newsletterImg').src = (window.innerWidth >= 1280) ? './assets/images/illustration-sign-up-desktop.svg' : './assets/images/illustration-sign-up-mobile.svg';
 }
 
-function isFormValid() {
-    const email = document.querySelector('#newsletter-register-email').value;
-    let emailError = document.querySelector('#newsletter-register-email-error');
+function validateForm() {
+    const emailError = document.querySelector('#newsletter-register-email-error');
+    const emailInput = document.querySelector('#newsletter-register-email');
+    const email = emailInput.value;
 
     if (email.length === 0 || email === null || email === undefined) {
-        emailError.classList.remove('hide');
-        emailError.textContent = 'Un email est requis pour l\'abonnement à la newsletter.';
+        const msg = 'Veuillez renseigner un email.';
+        displayError(emailInput, emailError, msg)
     }
     else if (!email.includes('@')) {
-        emailError.classList.remove('hide');
-        emailError.textContent = 'L\'email saisi est incorrect. Vérifiez qu`il contient un arobase.';
+        const msg = 'L\'email saisi est invalide.';
+        displayError(emailInput, emailError, msg);
     }
     else if (email.includes('@') && email.substring(email.length-4, email.length) === ".com") {
-        window.location.href = 'https://www.youtube.com';
+        hideError(emailInput, emailError);
     }
+}
+
+function displayError(inputElement, errorElement, msg) {
+    inputElement.classList.add('error');
+    errorElement.classList.remove('hide');
+    errorElement.textContent = msg;
+}
+function hideError(inputElement, errorElement) {
+    inputElement.classList.remove('error');
+    errorElement.classList.add('hide');
 }
